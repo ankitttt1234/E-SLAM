d9af6f379f88e7d7770ffa6f1402d5ce5a058091
ankitttt___ <ankitdoddi@gmail.com>
2020-07-12 16:11:51 +0530

    port changes

--- index.js
+++ index.js
@@ -147,7 +147,10 @@ app.get('/auth/facebook',
 
   
 
-app.listen(5000,()=>{
-    console.log("Listening on port 5000")
-})
- 
+  let port=process.env.PORT;
+
+  if(port==null || port==""){
+    port=5000;
+  }
+  console.log(port);
+  app.listen(port, () => console.log(`Server started on port ${port}`));
